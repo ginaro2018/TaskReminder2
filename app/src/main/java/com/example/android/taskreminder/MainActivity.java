ab9900feb5f04b7cee5ac30e4edd2115bbee3e55
GINARO <35197121+ginaro2018@users.noreply.github.com>
2018-02-08 20:20:56 +0300

    Added datepicker methods
    1.myDate() method,
    2.myDate() method

--- app/src/main/java/com/example/android/taskreminder/MainActivity.java
+++ app/src/main/java/com/example/android/taskreminder/MainActivity.java
@@ -60,5 +60,65 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // the date method
+    public void myDate() {
+        date.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // calender class's instance and get current date , month and year from calender
+                        final Calendar c = Calendar.getInstance();
+                        int mYear = c.get( Calendar.YEAR ); // current year
+                        int mMonth = c.get( Calendar.MONTH ); // current month
+                        int mDay = c.get( Calendar.DAY_OF_MONTH ); // current day
+                        // date picker dialog
+                        datePickerDialog = new DatePickerDialog( MainActivity.this,
+                                new DatePickerDialog.OnDateSetListener() {
+
+                                    @Override
+                                    public void onDateSet(DatePicker view, int year,
+                                                          int monthOfYear, int dayOfMonth) {
+                                        // set day of month , month and year value in the edit text
+                                        date.setText( dayOfMonth + "/"
+                                                + (monthOfYear + 1) + "/" + year );
+
+                                    }
+                                }, mYear, mMonth, mDay );
+                        datePickerDialog.show();
+                    }
+                } );
+    }
+
+    //calling myDate function in datePicker2
+    public void myDate2() {
+        dateExpired.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // calender class's instance and get current date , month and year from calender
+                        final Calendar c = Calendar.getInstance();
+                        int mYear = c.get( Calendar.YEAR ); // current year
+                        int mMonth = c.get( Calendar.MONTH ); // current month
+                        int mDay = c.get( Calendar.DAY_OF_MONTH ); // current day
+                        // date picker dialog
+                        datePickerDialog = new DatePickerDialog( MainActivity.this,
+                                new DatePickerDialog.OnDateSetListener() {
+
+                                    @Override
+                                    public void onDateSet(DatePicker view, int year,
+                                                          int monthOfYear, int dayOfMonth) {
+                                        // set day of month , month and year value in the edit text
+                                        dateExpired.setText( dayOfMonth + "/"
+                                                + (monthOfYear + 1) + "/" + year );
+
+                                    }
+                                }, mYear, mMonth, mDay );
+                        datePickerDialog.show();
+                    }
+                } );
+
+    }
+    
+    
 
 }
