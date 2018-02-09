401afaaf997bdff60297a725ed4b50e8b502b7b6
Xilma <atiruo@gmail.com>
2018-02-09 20:01:21 +0300

    Layout interface color and button change
    Added an extra column in database i.e. split name into first name and last name

---
+++ src/main/java/com/example/android/taskreminder/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.android.taskreminder;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity {
+    DbHelper myDb;
+    EditText lname, fname, email, phone, policyType, dateissued, dateExpired;
+    Button btnAddClient, delButton;
+    EditText date;
+    DatePickerDialog datePickerDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        myDb = new DbHelper( this );
+
+
+        // initialize the editTexts and buttons(casting)
+        fname = (EditText) findViewById( R.id.editText1 );
+        lname = (EditText) findViewById( R.id.editText15 );
+        email = (EditText) findViewById( R.id.editText2 );
+        phone = (EditText) findViewById( R.id.editText3 );
+        policyType = (EditText) findViewById( R.id.editText4 );
+        dateissued = (EditText) findViewById( R.id.datepicker1 );
+        dateExpired = (EditText) findViewById( R.id.datepicker2 );
+        //buttons
+        btnAddClient = (Button) findViewById( R.id.button_submit );
+        delButton = (Button) findViewById( R.id.buttonSrc );
+
+        //calling the AddClient method inside onCreate method
+        AddClient();
+        myDate();
+        myDate2();
+    }
+
+    /*
+//defining  the date method
+    private String getDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String date = sdf.format(new Date());
+    }  */
+
+    // method to add clients
+    public void AddClient() {
+        btnAddClient.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData( fname.getText().toString(), lname.getText().toString(), email.getText().toString(), phone.getText().toString(),
+                                policyType.getText().toString(), dateissued.getText().toString(), dateExpired.getText().toString() );
+                        if (isInserted = true)
+                            Toast.makeText( MainActivity.this, "Data Inserted", Toast.LENGTH_LONG ).show();
+                        else
+                            Toast.makeText( MainActivity.this, "Data NOT Inserted", Toast.LENGTH_LONG ).show();
+                    }
+                }
+        );
+
+    }
+
+    // the datepicker1 method
+   public void myDate() {
+        date.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // calender class's instance and get current date , month and year from calender
+                        final Calendar c = Calendar.getInstance();
+                        int mYear = c.get( Calendar.YEAR ); // current year
+                        int mMonth = c.get( Calendar.MONTH ); // current month
+                        int mDay = c.get( Calendar.DAY_OF_MONTH ); // current day
+                        // date picker dialog
+                        datePickerDialog = new DatePickerDialog( MainActivity.this,
+                                new DatePickerDialog.OnDateSetListener() {
+
+                                    @Override
+                                    public void onDateSet(DatePicker view, int year,
+                                                          int monthOfYear, int dayOfMonth) {
+                                        // set day of month , month and year value in the edit text
+                                        date.setText( dayOfMonth + "/"
+                                                + (monthOfYear + 1) + "/" + year );
+
+                                    }
+                                }, mYear, mMonth, mDay );
+                        datePickerDialog.show();
+                    }
+                } );
+    }
+
+    //calling myDate function in datePicker2
+    public void myDate2() {
+        dateExpired.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // calender class's instance and get current date , month and year from calender
+                        final Calendar c = Calendar.getInstance();
+                        int mYear = c.get( Calendar.YEAR ); // current year
+                        int mMonth = c.get( Calendar.MONTH ); // current month
+                        int mDay = c.get( Calendar.DAY_OF_MONTH ); // current day
+                        // date picker dialog
+                        datePickerDialog = new DatePickerDialog( MainActivity.this,
+                                new DatePickerDialog.OnDateSetListener() {
+
+                                    @Override
+                                    public void onDateSet(DatePicker view, int year,
+                                                          int monthOfYear, int dayOfMonth) {
+                                        // set day of month , month and year value in the edit text
+                                        dateExpired.setText( dayOfMonth + "/"
+                                                + (monthOfYear + 1) + "/" + year );
+
+                                    }
+                                }, mYear, mMonth, mDay );
+                        datePickerDialog.show();
+                    }
+                } );
+
+    }
+    
+
+}
